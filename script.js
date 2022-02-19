e8846488268f3fe886d621433d45c50b7b991eac
Belvin Benny <belvinbbenny@gmail.com>
2022-02-19 08:27:04 +0530

    changes made in form

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-function validateForm() {
-    let x = document.forms["myForm"]["name"].value;
-    var spaces=  /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-    else if(x.match(spaces)){
-        alert("Invalid");
-        return false;
+function clearErrors(){
+  errors=document.getElementsByClassName('formerror');
+  for(let item of errors){
+    item.innerHTML=" ";
+  }
+}
 
-    }else{
-        return true;
-    }
-  } 
+function seterror(id,error){
+  //sets an error inside the id tag
+  element = document.getElementById(id);
+  element.getElementsByClassName('formerror')[0].innerHTML=error;
+
+}
+
+
+function validateForm(){
+  //for validation purpose
+  var returnval = true;
+  clearErrors();
+
+  var name = document.forms['myForm']["fname"].value;
+  if(name.length==0){
+    seterror("name","Name cannot be empty");
+    returnval = false;
+  }
+
+  var email= document.forms['myForm']["femail"].value;
+  if(email.length>25){
+    seterror("email","email length is too long");
+    returnval = false;
+  }
+
+  var phone= document.forms['myForm']["fphone"].value;
+  if(phone.length!=10){
+    seterror("phone","Phone number should be 10 digits long");
+    returnval = false;
+  }
+
+  
+
+  return returnval;
+
+
+}
